d306cbbd8cd3799da2538baa8396a8798dbc2e2e
jianmin li <jian.min.li@hotmail.com>
2016-08-28 10:14:39 +0800

    committed (just try)

--- JianminGit/src/com/jm/test/HelloWorld.java
+++ JianminGit/src/com/jm/test/HelloWorld.java
@@ -4,7 +4,11 @@ public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello world 1");
 		System.out.println("Hello world 2");
+<<<<<<< Upstream, based on origin/branch-1#2
 		System.out.println("Hello world 3");
 		System.out.println("edit from branch 1");
+=======
+		System.out.println("edit in main");
+>>>>>>> c6e221f edit master
 	}
 }
